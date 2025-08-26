c22aa21cebeba8c63d61c829ede5c22ab8470644
Nikhil G S <gsnikhil333@gmail.com>
2025-08-25 22:40:24 -0700

    add frontend server path

--- frontend/src/socket.ts
+++ frontend/src/socket.ts
@@ -5,4 +5,5 @@ let VITE_BACKEND_URL = import.meta.env.VITE_BACKEND_URL;
 if (!VITE_BACKEND_URL) {
   VITE_BACKEND_URL = "http://localhost:3000";
 }
+console.log("Connecting to backend at:", VITE_BACKEND_URL);
 export const socket = io(VITE_BACKEND_URL);
